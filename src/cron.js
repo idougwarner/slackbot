5059c81aa34c62aaa0f5aa616b3b64646c1d802c
Doug Warner <idougwarner@gmail.com>
2022-01-04 14:52:43 -0800

    update cronjob

--- src/cron.js
+++ src/cron.js
@@ -40,17 +40,17 @@ const syncDataOverUserPermission = async (userId) => {
   }
 };
 
-// cron.schedule('*/5 * * * *', async () => {
-//   console.log('syncing ...');
+cron.schedule('*/5 * * * *', async () => {
+  console.log('syncing ...');
 
-//   await syncPicklistValues();
+  await syncPicklistValues();
 
-//   const users = localStorage.getItem('users');
-//   const promises = users.map(syncDataOverUserPermission);
-//   await Promise.all(promises);
-// });
+  const users = localStorage.getItem('users');
+  const promises = users.map(syncDataOverUserPermission);
+  await Promise.all(promises);
 
-syncPicklistValues();
+  console.log('sync done');
+});
 
 module.exports = {
   syncPicklistValues,
